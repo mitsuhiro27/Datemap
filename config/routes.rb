9af299c01652c2b42ee40071d145abce13ee3769
高橋光洋 <takahashimitsuhiro@takahashimitsuhironoMacBook-Pro.local>
2023-07-19 23:20:11 +0900

    devise日本語化

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-    :registrations => 'users/registrations',
-    :sessions => 'users/sessions'   
-  } 
-
+  devise_for :users
   devise_scope :user do
-    get "user/:id", :to => "users/registrations#detail"
-    get "signup", :to => "users/registrations#new"
-    get "login", :to => "users/sessions#new"
-    get "logout", :to => "users/sessions#destroy"
+    root "users/sessions#new"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
