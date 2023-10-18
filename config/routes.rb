d7fa800396740ba0b8d411519c1b0e05a8039efe
高橋光洋 <takahashimitsuhiro@takahashimitsuhironoMacBook-Pro.local>
2023-10-18 23:59:15 +0900

    feat: GoogleMapを表示する

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: "users/registrations"}
   devise_scope :user do
-    root "users/sessions#new"
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
   resources :users, :only => [:show]
   resources :posts
   resources :posts do
     resource :favorites, only: [:create, :destroy]
- end
+  end
+  root "maps#index"
+  resources :maps, only: [:index]
 end
