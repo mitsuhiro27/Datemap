ad42e9adc93fa6b55d70e6e0834da4e5cd7ad47d
高橋光洋 <takahashimitsuhiro@takahashimitsuhironoMacBook-Pro.local>
2023-11-27 00:14:18 +0900

    test: rspec設定

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,11 @@
 require 'rails_helper'
-
 RSpec.describe User, type: :model do
-  it "名前とメールアドレスとパスワードがあれば登録できる"
-
-  it "名前がなければ登録できない" do
-    let(:user) { User.new(**params) }
-    let(:params) { { name: '' } }
-    expect
-
-  it "メールアドレスがなければ登録できない"
-
-  it "メールアドレスが重複していたら登録できない"
-
-  it "パスワードがなければ登録できない"
-
-  it "パスワードが暗号化されているか"
-
-
+  it "名、メール、パスワードがある場合、有効である" do
+    user = User.new(
+    name: "tarou",
+    email: "testman@example.com",
+    password: "password",
+    )
+    expect(user).to be_valid
+  end
 end
